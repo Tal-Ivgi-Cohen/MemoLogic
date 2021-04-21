6c8ef4fd6f5dfaafff5843085619500d59ab33db
Tal-Ivgi <tal.ivgi@gmail.com>
2021-04-21 15:45:40 +0300

    testa

--- js/memologic.controller.js
+++ js/memologic.controller.js
@@ -129,7 +129,8 @@ function uploadImg(elForm, ev) {
         <button class="canvas-btn"><a class="btn" href="./https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('./https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
         <img src="icon/facebook-logo.PNG" width=15 heigh=15/>   
         </a></button>
-
+        <button class="canvas-btn">   <a href="whatsapp://send?text=${uploadedImgUrl}&t=${uploadedImgUrl}" data-action="share/whatsapp/share"  
+        target="_blank"> <img src="./icon/whatsapp.PNG" width=20 heigh=20/> </a>  </button> 
         `
     }
     doUploadImg(elForm, onSuccess);
@@ -137,7 +138,7 @@ function uploadImg(elForm, ev) {
 
 function doUploadImg(elForm, onSuccess) {
     var formData = new FormData(elForm);
-    fetch('./http://ca-upload.com/here/upload.php', {
+    fetch('http://ca-upload.com/here/upload.php', {
             method: 'POST',
             body: formData
         })
