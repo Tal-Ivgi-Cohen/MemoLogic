1d10e2d7fa0d29d3608b9d687334ad6372cabdb5
Tal-Ivgi <tal.ivgi@gmail.com>
2021-04-21 18:24:38 +0300

    facebook

--- js/memologic.controller.js
+++ js/memologic.controller.js
@@ -130,8 +130,7 @@ function uploadImg(elForm, ev) {
     function onSuccess(uploadedImgUrl) {
         uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
         document.querySelector('.share-container').innerHTML = `
-        <button class="canvas-btn"><a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
-        <img src="icon/facebook-logo.PNG" width=15 heigh=15/>   
+        <button class="canvas-btn fa facebook"><a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">   
         </a></button>
         <button><a href="whatsapp://send?text=${uploadedImgUrl}&t=${uploadedImgUrl}" data-action="share/whatsapp/share" target="_blank"> <img src="icon/whatsapp.PNG" width=20 heigh=20/> </a></button> 
         `
